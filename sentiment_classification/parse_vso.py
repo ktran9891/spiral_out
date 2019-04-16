cb550d2ec9910c29492d4fa2bda47cfb6c7a8057
Kevin Tran <ktrain9891@gmail.com>
2019-04-16 00:06:47 -0400

    Parse VSO into individual pickles

--- sentiment_classification/parse_vso.py
+++ sentiment_classification/parse_vso.py
@@ -1,18 +1,21 @@
 import os
+import gc
 import tempfile
 import urllib
 import cv2
+import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
 
 # Administrative constants
 vso_dir = './vso/'
-h5_file_name = 'vso.h5'
+pickle_dir = './parsed_vso/'
 
 
-def process_anps(anp_file_name):
-    # Get the ANP from the file name
+# Get the ANP from the file names
+file_names = os.listdir(vso_dir)
+for anp_file_name in tqdm(file_names, desc='ANPs'):
     adj = anp_file_name.split('_')[0]
     noun = anp_file_name.split('_')[1].split('.')[0]
 
@@ -21,21 +24,19 @@ def process_anps(anp_file_name):
     urls = []
     with open(vso_dir + anp_file_name, 'rb') as file_handle:
         file_lines = file_handle.readlines()
-    #for line in tqdm(file_lines, total=len(file_lines), desc='Images for this ANP'):
-    for line in file_lines:
+    for line in tqdm(file_lines, total=len(file_lines), desc='Images for this ANP'):
         url = str(line).split(' ')[1]
 
         # Write the image to a temporary file and then get the RGB from it
         _, img_path = tempfile.mkstemp()
         try:
-            urllib.request.urlretrieve(url, img_path)
+            urllib.urlretrieve(url, img_path)
             img = cv2.imread(img_path)
-            imgs.append(img)
-            urls.append(url)
 
-        # Ignore broken links
-        except urllib.error.HTTPError:
-            pass
+            # Ignore broken links
+            if img is not None:
+                imgs.append(img)
+                urls.append(url)
 
         # Delete the temporary file
         finally:
@@ -47,19 +48,13 @@ def process_anps(anp_file_name):
     # Report some 404 errors
     print('Got %i out of %i images for %s_%s (%i%%)'
           % (len(imgs), len(file_lines), adj, noun,
-             len(imgs)/len(file_lines)*100))
+             len(imgs)/len(file_lines)*100.))
 
     # Save the results
-    data_dict = {'Adjective': [adj for _ in imgs],
-                 'Noun': [noun for _ in imgs],
-                 'RGB': [img for img in imgs],
-                 'URL': [url for url in urls]}
+    data_dict = {'URL': urls,
+                 'RGB': imgs}
     df = pd.DataFrame(data_dict)
-    df.to_hdf(h5_file_name, 'VSO', append=True)
+    df.to_pickle(pickle_dir + '%s_%s.pkl' % (adj, noun))
 
-
-# get all of the attribute-noun-pairs in Columbia's visual sentiment ontology
-# (VSO) dataset
-file_names = os.listdir(vso_dir)
-iterator = map(process_anps, file_names)
-list(tqdm(iterator, total=len(file_names), desc='ANPs'))
+    # Collect the garbage just in case we run into memory errors
+    gc.collect()
