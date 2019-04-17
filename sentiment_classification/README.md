eef1f45ffdee77fbb143770186d405fd50ecc788
Kevin Tran <ktrain9891@gmail.com>
2019-04-17 12:51:31 -0400

    Download and split VSO in one step

--- sentiment_classification/README.md
+++ sentiment_classification/README.md
@@ -1,2 +1,6 @@
-The images were obtained from
-[VSO](http://www.ee.columbia.edu/ln/dvmm/vso/download/flickr_dataset.html).
+The contents of the `vso` folder were obtained from
+[VSO](http://www.ee.columbia.edu/ln/dvmm/vso/download/flickr_dataset.html). I
+downloaded the database using the `download_vso.py` file, which split and saved
+the images in their respective `train/` and `test/` folders. Note that the
+numbering in each of the folders is specific to that folder---e.g.,
+`train/spooky_house_1.jpg` and `test/spooky_house_1.jpg` are different images.
--- sentiment_classification/download_vso.py
+++ sentiment_classification/download_vso.py
@@ -2,62 +2,59 @@ import os
 import tempfile
 import urllib
 import cv2
-import pickle
 import multiprocess
-import pandas as pd
 from tqdm import tqdm
+from sklearn.model_selection import train_test_split
 
 
 # Administrative constants
 vso_dir = './vso/'
-pickle_dir = './parsed_vso/'
+training_dir = './train/'
+test_dir = './test/'
+train_size = 0.8
 
 
 def parse_anp_images(anp_file_name):
     adj = anp_file_name.split('_')[0]
     noun = anp_file_name.split('_')[1].split('.')[0]
-    pickle_file = pickle_dir + '%s_%s.pkl' % (adj, noun)
 
-    # Only parse it if the file does not already exist
-    if not os.path.isfile(pickle_file):
+    # Download each image listed in the file for this ANP
+    imgs = []
+    with open(vso_dir + anp_file_name, 'rb') as file_handle:
+        file_lines = file_handle.readlines()
+    for line in tqdm(file_lines, total=len(file_lines), desc='%s_%s' % (adj, noun)):
+        url = str(line).split(' ')[1]
 
-        # Download each image listed in the file
-        imgs = []
-        urls = []
-        with open(vso_dir + anp_file_name, 'rb') as file_handle:
-            file_lines = file_handle.readlines()
-        for line in tqdm(file_lines, total=len(file_lines), desc='%s_%s' % (adj, noun)):
-            url = str(line).split(' ')[1]
+        # Write the image to a temporary file and then get the RGB from it
+        _, img_path = tempfile.mkstemp()
+        try:
+            urllib.request.urlretrieve(url, img_path)
+            img = cv2.imread(img_path)
+            imgs.append(img)
 
-            # Write the image to a temporary file and then get the RGB from it
-            _, img_path = tempfile.mkstemp()
+        # Ignore broken links
+        except urllib.error.HTTPError:
+            pass
+
+        # Delete the temporary file
+        finally:
             try:
-                urllib.request.urlretrieve(url, img_path)
-                img = cv2.imread(img_path)
-                imgs.append(img)
-                urls.append(url)
-
-            # Ignore broken links
-            except urllib.error.HTTPError:
+                os.remove(img_path)
+            except OSError:
                 pass
 
-            # Delete the temporary file
-            finally:
-                try:
-                    os.remove(img_path)
-                except OSError:
-                    pass
+    # Split the images
+    train_imgs, test_imgs = train_test_split(imgs, train_size=train_size, test_size=1-train_size)
 
-        # Save the results
-        data = {'URL': urls,
-                'RGB': imgs}
-        df = pd.DataFrame(data)
-        with open(pickle_file, 'wb') as file_handle:
-            pickle.dump(df, file_handle)
+    # Save the images
+    for i, img in enumerate(train_imgs):
+        cv2.imwrite(training_dir + '%s_%s_%i.jpg' % (adj, noun, i), img)
+    for i, img in enumerate(test_imgs):
+        cv2.imwrite(test_dir + '%s_%s_%i.jpg' % (adj, noun, i), img)
 
 
-# Get the ANP from the file names
+# Download and split all images for all ANPs
 file_names = os.listdir(vso_dir)
-with multiprocess.Pool(272) as pool:
+with multiprocess.Pool(272, maxtasksperchild=1) as pool:
     iterator = pool.imap(parse_anp_images, file_names)
     list(tqdm(iterator, total=len(file_names), desc='All ANPs'))
