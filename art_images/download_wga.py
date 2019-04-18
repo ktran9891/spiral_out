b112979bac41cf3171a45c55e3c74f52f8dd9544
Kevin Tran <ktrain9891@gmail.com>
2019-04-18 12:47:32 -0400

    Download WGA images

---
+++ art_images/download_wga.py
@@ -0,0 +1,25 @@
+import urllib
+import pandas as pd
+import multiprocess
+from tqdm import tqdm
+
+
+# Read the catalog
+df = pd.read_csv('catalog.txt', sep='\t', lineterminator='\r')
+# Take out the last data point, which is broken for some reason
+df = df.drop([df.shape[0]-1])
+# Rip out the URLs
+urls = df['URL']
+
+
+# Create a function to download an image based on its index
+def download_image(index):
+    url = urls[index]
+    image_url = url.replace('html', 'art', 1).replace('html', 'jpg', 1)
+    urllib.request.urlretrieve(image_url, './images/%05d.jpg' % index)
+
+
+# Multithreaded downloading
+with multiprocess.Pool(272, maxtasksperchild=1000) as pool:
+    iterator = pool.imap(download_image, range(len(urls)))
+    list(tqdm(iterator, total=len(urls)))
