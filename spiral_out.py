68dd2f6868afcdd5eddc94ab2c3428d1ebf4dd29
Kevin Tran <ktrain9891@gmail.com>
2019-04-22 11:33:52 -0400

    Add image names to history pickle and make smaller test case

--- spiral_out.py
+++ spiral_out.py
@@ -12,14 +12,27 @@ from dragonfly import maximise_function
 from style_transfer import transfer_style
 
 
-art_folder = '../art_images/'
+# Hyperparametercs
+seed_image = './experimental_results/me.jpg'
+attribute = 'happy'
+max_capital = 10
+
+
+# Data and results folders
+experiment_folder = ('./experimental_results/%s_%s/'
+                     % (attribute, seed_image.split('/')[-1].split('.')[0]))
+try:
+    os.mkdir(experiment_folder)
+except OSError:
+    pass
+art_folder = './art_images/'
 
 # Load the classifier
-classifier = load_model('../sentiment_classification/resnet50_vso.h5')
+classifier = load_model('./sentiment_classification/resnet50_vso.h5')
 
 # Initialize a dummy data generator so we can get our attribute labeling map
 dummy_datagen = ImageDataGenerator()
-dummy_gen = dummy_datagen.flow_from_directory('../sentiment_classification/train/',
+dummy_gen = dummy_datagen.flow_from_directory('./sentiment_classification/train/',
                                               class_mode='categorical')
 attr_map = dummy_gen.class_indices
 
@@ -40,7 +53,7 @@ def calc_attribute_match(encoded_feature_vector, attribute):
 def update_style(encoded_feature_vector):
     # Increment the counter for specifying which folder to save the images in
     global updates
-    results_folder = './style_transfer_%03d' % updates
+    results_folder = experiment_folder + 'style_transfer_%03d' % updates
     updates += 1
     # Make the directory to store the style transfer progression (EAFP)
     try:
@@ -60,10 +73,6 @@ def update_style(encoded_feature_vector):
     current_image = transfer_style(current_image, style_path,
                                    result_folder=results_folder,
                                    show=False)
-
-    # Update the list of style images we've been using
-    global style_images
-    style_images.append(style_image)
 
     # Remove the style image from the list of candidates (to avoid redos)
     del wga_names[image_index]
@@ -97,17 +106,10 @@ def obj_fun(probs, attribute):
     return match
 
 
-# Hyperparametercs
-seed_image = 'me.jpg'
-attribute = 'happy'
-max_capital = 100
-
-
 # Load image names and initialize the list of style images
 with open(art_folder + 'image_mapping.txt', 'r') as file_handle:
     wga_names = file_handle.readlines()
 wga_names = [name.split('\n')[0] for name in wga_names]
-style_images = []
 
 # Read the encoded features, then make a KDTree from them for matching
 h5f = h5py.File(art_folder + 'features.h5', 'r')
@@ -131,5 +133,5 @@ max_val, max_pt, history = maximise_function(lambda features: calc_attribute_mat
 
 # Save it before we lose it!
 seed_name = seed_image.split('.')[0]
-with open('history_for_%s_%s.pkl' % (attribute, seed_name), 'wb') as file_handle:
+with open(experiment_folder + 'history.pkl', 'wb') as file_handle:
     pickle.dump((max_val, history, style_images), file_handle)
