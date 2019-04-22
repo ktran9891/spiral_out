72978f94ed4f0dea34e7d2dfcd01bc4555ce3589
Kevin Tran <ktrain9891@gmail.com>
2019-04-21 20:14:12 -0400

    Reduce encoder latent size from 8 to 4

--- art_images/encode_features.py
+++ art_images/encode_features.py
@@ -44,7 +44,7 @@ def train_encoder(x_train, x_validate,
     return autoencoder, encoder
 
 # Train shallow [auto]encoder
-n_latent_vars = 8
+n_latent_vars = 4
 autoencoder, encoder = train_encoder(x_train=features_train,
                                      x_validate=features_validate,
                                      n_latent_vars=n_latent_vars,
@@ -64,5 +64,9 @@ encoder.save('encoder.h5')
 # And then save the encoded features
 encoded_features = encoder.predict(features)
 h5f = h5py.File('features.h5', 'a')
-h5f.create_dataset('encoded_features', data=features)
+try:
+    h5f.create_dataset('encoded_features', data=features)
+except RuntimeError:
+    h5_data = h5f['encoded_features']
+    h5_data = features
 h5f.close()
