4ecf51c8e6a819f337bb32630b1d62bf35085ae4
Kevin Tran <ktrain9891@gmail.com>
2019-04-27 16:40:23 -0400

    Make a function out of the gif creator

--- experimental_results/create_gifs.py
+++ experimental_results/create_gifs.py
@@ -1,24 +1,32 @@
 import os
+import sys
 import imageio
 
 
-# Find each ANP
-folders = [folder for folder in os.listdir('.')
-           if '.' not in folder and folder != 'archive']
-for folder in folders:
+# Input arguments
+attr = sys.argv[1]
+seed = sys.argv[2]
+try:
+    fps = sys.argv[3]
+except IndexError:
+    fps = 20
 
-    # Find each style transfer iteration
-    subfolders = [os.path.join(folder, subfolder)
-                  for subfolder in os.listdir(folder)
-                  if '.' not in subfolder]
-    subfolders.sort()
 
-    # Find each image
-    file_paths = [os.path.join(subfolder, file_path)
-                  for subfolder in subfolders
-                  for file_path in os.listdir(subfolder)]
-    file_paths.sort()
+# Find each style transfer iteration
+folder = attr + '_' + seed.split('.')[0]
+subfolders = [os.path.join(folder, subfolder)
+              for subfolder in os.listdir(folder)
+              if '.' not in subfolder]
+subfolders.sort()
 
-    # Put the images into a GIF
-    images = [imageio.imread(path) for path in file_paths]
-    imageio.mimsave(folder + '.gif', images, fps=20)
+# Find each image
+file_paths = [os.path.join(subfolder, file_path)
+              for subfolder in subfolders
+              for file_path in os.listdir(subfolder)]
+file_paths.sort()
+
+# Start the GIF with the native image
+images = [imageio.imread(seed) for _ in range(int(fps))]
+# Add the style transfer images to the GIF
+images.extend([imageio.imread(path) for path in file_paths])
+imageio.mimsave(folder + '.gif', images, fps=fps)
