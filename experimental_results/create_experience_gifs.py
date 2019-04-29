6871d89a7e9cb3785f35e61cc5a97457b17ba8a4
Kevin Tran <ktrain9891@gmail.com>
2019-04-29 13:31:21 -0400

    Make content within experience GIFs centered

--- experimental_results/create_experience_gifs.py
+++ experimental_results/create_experience_gifs.py
@@ -3,6 +3,7 @@ import sys
 import pickle
 import numpy as np
 import imageio
+from PIL import Image
 
 
 # Input arguments
@@ -12,6 +13,7 @@ try:
     fps = sys.argv[3]
 except IndexError:
     fps = 20
+size = (1024, 1024)
 
 
 # Find the style images
@@ -24,14 +26,21 @@ art_folder = '../art_images/images/'
 file_paths = [os.path.join(art_folder, file_name)
               for file_name in style_images]
 
-# Start the GIF with a black image to go along with the OG portrait
-#shape = imageio.imread(seed).shape
-#black = np.zeros(shape, dtype=np.uint8)
-#images = [black for _ in range(int(fps))]
+# Add the experience images to the GIF
 images = []
+for path in file_paths:
+    # Read each experience image
+    image = Image.open(path)
+    image.thumbnail(size, Image.ANTIALIAS)
 
-# Add the experienc images to the GIF
-images.extend([imageio.imread(path)
-               for path in file_paths
-               for _ in range(int(fps))])
+    # Center and pad the image
+    offset_x = max((size[0] - image.size[0]) / 2, 0)
+    offset_y = max((size[1] - image.size[1]) / 2, 0)
+    offsets = (offset_x, offset_y)
+    thumbnail = Image.new(mode='RGB', size=size, color=(1, 1, 1))
+    thumbnail.paste(image, offsets)
+
+    # Save a full set of frames per image
+    for _ in range(int(fps)):
+        images.append(thumbnail)
 imageio.mimsave(folder + '_experiences.gif', images, fps=fps)
